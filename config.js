80262e1d1315c6231482c0b7eee00974ecc20ba2
txtsd <code@ihavea.quest>
2022-11-09 19:09:15 +0530

    chore: Fix grammar
    Signed-off-by: txtsd <code@ihavea.quest>

--- config.js
+++ config.js
@@ -302,7 +302,7 @@ Number.prototype.div = function (divisor) {
 
 
 function LevelUpTime(level) {  // seconds
-  // 20 minutes for for level 1
+  // 20 minutes for level 1
   // exponential increase after that
   return Math.round((20 + Math.pow(1.15, level)) * 60);
 }
